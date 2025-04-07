34849375056ebf3f7832a523c75a53e1bde47371
Alex Brelsford <alexandriabrelsford@gmail.com>
2025-04-06 23:09:18 -0400

    Update index.js

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ initiateWorkerPool = () => {
 // Socket
 openWebSocket = () => {
     const WebSocketClient = require('./WebsocketClient')
-    const wss = new WebSocketServer.Server({ port: 8080 })
+    const wss = new WebSocketServer.Server(Config.token)
     let initialPacket = { "op": "authenticate", "token": Config.token }
     wss.onOpen = (_) => { wss.send(JSON.stringify(initialPacket)); }
     wss.onClose = (e) => { console.log('SimplyWS/onClose :: %s', e); e = '' }
